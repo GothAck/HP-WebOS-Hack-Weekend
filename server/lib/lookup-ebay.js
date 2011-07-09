081059ebf51b8741853475691203db776fa212d4
proto <g.mackelden@civicboom.com>
2011-07-09 16:28:36 +0100

    Ebay plugin returns object containing 3 item results

--- server/lib/lookup-ebay.js
+++ server/lib/lookup-ebay.js
@@ -1,26 +1,42 @@
 // Any requires here
 
 var http = require ('http');
+var querystring = require('querystring');
 
 function search (term, callback) {
   http.get(
     {
-      host: 'localhost',
+      host: 'svcs.ebay.com',
       port: 80,
-      path: '/'
+      path: api_call_url(term, 3)
     },
     function (response) {
       var data = '';
       response.on('data', function (chunk) { data += chunk })
       response.on('end', function () {
-        //process data here
-        //var object = JSON.parse (data);
-        var myResultObjects = [];
+        // process data here
+        var myResultObjects = JSON.parse(data);
         callback(false, myResultObjects) //If there is an error, callback with true as first value and data = error string
       });
     }).on('error', function (error) {
       callback(true, 'HTTP Error');
     });
+}
+
+// Makes keyword search url using given paramater string
+function api_call_url(keywords, results) {
+  var base_url = "/services/search/FindingService/v1?";
+  var appid = "GMackeld-40b4-41b0-8799-c897cec58896"; 
+  var apicall = base_url + querystring.stringify({
+    "OPERATION-NAME": "findItemsByKeywords",
+    "SERVICE-VERSION":  "1.0.0",
+    "SECURITY-APPNAME": appid,
+    "GLOBAL-ID":  "EBAY-GB",
+    "RESPONSE-DATA-FORMAT": "JSON",
+    "keywords": keywords,
+    "paginationInput.entriesPerPage": results
+  }) + "&REST-PAYLOAD";
+  return apicall;
 }
 
 exports.search = search;
